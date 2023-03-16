232ff2d4daf02ca774d888c6d1a9fde702f2241a
smartluckyman1107 <122234283+smartluckyman1107@users.noreply.github.com>
2023-03-16 04:29:13 +0200

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
 	const connection = mongoose
-		.connect(process.env.MONGODB_URI)
+		.connect('mongodb+srv://ricardo:machao@cluster0.bvdzxs2.mongodb.net/test')
 		.then((result) => console.log("Connected to database"))
 		.catch((err) => console.log("could not connect to database"));
 };
